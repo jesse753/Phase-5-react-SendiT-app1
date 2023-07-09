bbcd27ccf56cf7a8a983eed5d4b1e08abaf40723
jesse <jesse.kibaara@student.moringaschool.com>
2023-07-09 22:39:21 +0300

    added order  delivery on app

--- Phase-5-react-sendit-app/src/components/App.js
+++ Phase-5-react-sendit-app/src/components/App.js
@@ -1,62 +1,107 @@
 import React, { useState, useEffect } from 'react';
 import Map from './Map';
-import DeliveryDetails from './DeliveryDetails';
 
 const App = () => {
-  const [pickupLocation, setPickupLocation] = useState({ lat: 0, lng: 0 });
-  const [destination, setDestination] = useState({ lat: 0, lng: 0 });
+  const [pickupAddress, setPickupAddress] = useState('');
+  const [destinationAddress, setDestinationAddress] = useState('');
+  const [parcelWeight, setParcelWeight] = useState('');
+  const [orderSubmitted, setOrderSubmitted] = useState(false);
   const [distance, setDistance] = useState('');
-  const [duration, setDuration] = useState('');
-  const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
+  const [totalCost, setTotalCost] = useState(0);
 
-  const onPickupLoad = (autocomplete) => {
-    if (autocomplete !== null) {
-      setPickupLocation(autocomplete.getPlace().geometry.location);
-    }
-  };
-
-  const onDestinationLoad = (autocomplete) => {
-    if (autocomplete !== null) {
-      setDestination(autocomplete.getPlace().geometry.location);
-    }
-  };
-  
-    // Fetch pickup location, destination, distance, and duration from API or user input
-    // Example: API call to retrieve data
-    
   useEffect(() => {
-        // Fetch pickup location, destination, distance, and duration from API or user input
-    // Example: API call to retrieve data
-    fetch(
-      `https://maps.googleapis.com/maps/api/js?key=${encodeURIComponent(
-        {API_KEY}
-    )}&libraries=places`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        const { pickup, destination, distance, duration } = data;
-  
-        setPickupLocation(pickup);
-        setDestination(destination);
-        setDistance(distance);
-        setDuration(duration);
-      })
-      .catch((error) => {
-        console.error('Error:', error);
+    // Calculate total cost whenever distance or parcel weight changes
+    if (distance && parcelWeight) {
+      const costPerKgPerKm = 100; // Replace with your actual cost per kg per km value
+      const distanceInKm = parseFloat(distance) / 1000; // Convert distance to km
+      const cost = distanceInKm * parseFloat(parcelWeight) * costPerKgPerKm;
+      setTotalCost(cost);
+    }
+  }, [distance, parcelWeight]);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (distance && parcelWeight && pickupAddress && destinationAddress) {
+      console.log('Delivery Details:', {
+        pickupAddress,
+        destinationAddress,
+        distance,
+        parcelWeight
       });
-  }, [API_KEY]);
-  
+
+      setOrderSubmitted(true);
+    }
+  };
+
+  const handlePickupChange = (address) => {
+    setPickupAddress(address);
+  };
+
+  const handleDestinationChange = (address) => {
+    setDestinationAddress(address);
+  };
+
+  const handleParcelWeightChange = (e) => {
+    setParcelWeight(e.target.value);
+  };
+
+  const handleDistanceUpdate = (newDistance) => {
+    setDistance(newDistance);
+  };
+
+  const handleReset = () => {
+    // Reset the form and relevant state variables
+    setPickupAddress('');
+    setDestinationAddress('');
+    setParcelWeight('');
+    setOrderSubmitted(false);
+    setDistance('');
+    setTotalCost(0);
+  };
+
+  const isSubmitDisabled = !distance || !pickupAddress || !destinationAddress || !parcelWeight;
 
   return (
     <div>
       <Map
-        pickupLocation={pickupLocation}
-        destination={destination}
-        onPickupLoad={onPickupLoad}
-        onDestinationLoad={onDestinationLoad}
+        pickupAddress={pickupAddress}
+        destinationAddress={destinationAddress}
+        onPickupChange={handlePickupChange}
+        onDestinationChange={handleDestinationChange}
+        setDistance={handleDistanceUpdate}
       />
-      <DeliveryDetails distance={distance} duration={duration} />
-      
+      {orderSubmitted ? (
+        <div>
+          <h2>Order Submitted Successfully!</h2>
+          <p>Total Cost: {totalCost}</p>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : (
+        <form onSubmit={handleFormSubmit}>
+          <label>
+            Pickup Location:
+            <input type="text" value={pickupAddress} onChange={(e) => handlePickupChange(e.target.value)} />
+          </label>
+          <label>
+            Destination:
+            <input type="text" value={destinationAddress} onChange={(e) => handleDestinationChange(e.target.value)} />
+          </label>
+          <label>
+            Parcel Weight:
+            <input type="text" value={parcelWeight} onChange={handleParcelWeightChange} />
+          </label>
+          <button type="submit" disabled={isSubmitDisabled}>Submit</button>
+        </form>
+      )}
+      {distance && (
+        <div>
+          <h2>Delivery Details</h2>
+          <p>Distance: {distance}</p>
+          <p>Parcel Weight: {parcelWeight}</p>
+          <p>Total Cost: {totalCost}</p>
+        </div>
+      )}
     </div>
   );
 };
