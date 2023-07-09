ef839366c4f046c42139ca52b3879c9c1ee528ec
jesse <jesse.kibaara@student.moringaschool.com>
2023-07-09 22:42:28 +0300

    added parcel weight , and total cost calcutaion matrix

--- Phase-5-react-sendit-app/src/components/Map.js
+++ Phase-5-react-sendit-app/src/components/Map.js
@@ -1,25 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import { GoogleMap, Marker, Polyline } from '@react-google-maps/api';
-import PlacesAutocomplete from 'react-places-autocomplete';
+import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
-const Map = ({ onPickupLoad, onDestinationLoad }) => {
-  const [pickupAddress, setPickupAddress] = useState('');
-  const [destinationAddress, setDestinationAddress] = useState('');
+const Map = ({ pickupAddress, destinationAddress, onPickupChange, onDestinationChange, setDistance }) => {
   const [pickupLocation, setPickupLocation] = useState(null);
   const [destination, setDestination] = useState(null);
-  const [distance, setDistance] = useState('');
+  const [distance, setDistanceValue] = useState('');
   const [duration, setDuration] = useState('');
 
-  const handlePickupChange = (address) => {
-    setPickupAddress(address);
-  };
+  useEffect(() => {
+    const fetchPickupLocation = async () => {
+      try {
+        const results = await geocodeByAddress(pickupAddress);
+        const latLng = await getLatLng(results[0]);
+        setPickupLocation(latLng);
+      } catch (error) {
+        console.error('Error fetching pickup location:', error);
+      }
+    };
 
-  const handleDestinationChange = (address) => {
-    setDestinationAddress(address);
-  };
+    const fetchDestination = async () => {
+      try {
+        const results = await geocodeByAddress(destinationAddress);
+        const latLng = await getLatLng(results[0]);
+        setDestination(latLng);
+      } catch (error) {
+        console.error('Error fetching destination:', error);
+      }
+    };
+
+    if (pickupAddress !== '') {
+      fetchPickupLocation();
+    }
+    if (destinationAddress !== '') {
+      fetchDestination();
+    }
+  }, [pickupAddress, destinationAddress]);
 
   const handleCalculate = () => {
-    // Use the Google Maps Distance Matrix API to calculate distance and duration
     const service = new window.google.maps.DistanceMatrixService();
     service.getDistanceMatrix(
       {
@@ -32,56 +50,15 @@ const Map = ({ onPickupLoad, onDestinationLoad }) => {
           const { distance, duration } = response.rows[0].elements[0];
           setDistance(distance.text);
           setDuration(duration.text);
+          setDistanceValue(distance.value);
         }
       }
     );
   };
 
-  useEffect(() => {
-    const fetchPickupLocation = async () => {
-      const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-          pickupAddress
-        )}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`
-      );
-      const data = await response.json();
-      if (data.results && data.results.length > 0) {
-        const { lat, lng } = data.results[0].geometry.location;
-        setPickupLocation({ lat, lng });
-      }
-    };
-
-    const fetchDestination = async () => {
-      try {
-        const response = await fetch(
-          `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
-            destinationAddress
-          )}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`
-        );
-        const data = await response.json();
-        console.log('Geocoding API response:', data);
-    
-        if (data.results && data.results.length > 0) {
-          const { lat, lng } = data.results[0].geometry.location;
-          setDestination({ lat, lng });
-        }
-      } catch (error) {
-        console.error('Error while fetching destination:', error);
-      }
-    };
-    
-
-    if (pickupAddress !== '') {
-      fetchPickupLocation();
-    }
-    if (destinationAddress !== '') {
-      fetchDestination();
-    }
-  }, [pickupAddress, destinationAddress]);
-
   return (
     <div>
-      <PlacesAutocomplete value={pickupAddress} onChange={(handlePickupChange)} onLoad={onPickupLoad}>
+      <PlacesAutocomplete value={pickupAddress} onChange={onPickupChange}>
         {({ getInputProps, suggestions, getSuggestionItemProps }) => (
           <div>
             <input {...getInputProps({ placeholder: 'Enter pickup location' })} />
@@ -96,7 +73,7 @@ const Map = ({ onPickupLoad, onDestinationLoad }) => {
         )}
       </PlacesAutocomplete>
 
-      <PlacesAutocomplete value={destinationAddress} onChange={handleDestinationChange} onLoad={onDestinationLoad}>
+      <PlacesAutocomplete value={destinationAddress} onChange={onDestinationChange}>
         {({ getInputProps, suggestions, getSuggestionItemProps }) => (
           <div>
             <input {...getInputProps({ placeholder: 'Enter destination' })} />
@@ -113,28 +90,22 @@ const Map = ({ onPickupLoad, onDestinationLoad }) => {
 
       <button onClick={handleCalculate}>Calculate</button>
 
-      {pickupLocation && destination && (
-        <GoogleMap
-          center={pickupLocation}
-          zoom={8}
-          mapContainerStyle={{ width: '100%', height: '400px' }}
-        >
-          <Marker position={pickupLocation} />
-          <Marker position={destination} />
-          
-          <Polyline
-            path={[pickupLocation, destination]}
-            options={{ strokeColor: '#0000FF' }}
-          />          
-      </GoogleMap>
-      )}
-
       {distance && duration && (
         <div>
-          <>DeliveryDetails</>
+          <h2>Delivery Details</h2>
           <p>Distance: {distance}</p>
           <p>Duration: {duration}</p>
         </div>
+      )}
+
+      {pickupLocation && destination && (
+        <GoogleMap center={pickupLocation} zoom={8} mapContainerStyle={{ width: '100%', height: '400px' }}>
+         
+          <Marker position={pickupLocation} />
+          <Marker position={destination} />
+
+          <Polyline path={[pickupLocation, destination]} options={{ strokeColor: '#0000FF' }} />
+        </GoogleMap>
       )}
     </div>
   );
